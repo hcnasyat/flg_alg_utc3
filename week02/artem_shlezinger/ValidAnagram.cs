ac116f3f1fe213f313ef71b9f7822d2edd44d150
GTECH\artem.s <artem.s@sbtech.com>
2021-02-16 20:02:52 +0200

    Elementary sorts [Selection, Insertion]

---
+++ week02/artem_shlezinger/ValidAnagram.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Algorithms.Sorts
+{
+    class ValidAnagram
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            char[] sArr = s.ToCharArray();
+            char[] tArr = t.ToCharArray();
+
+            if (SelectionSort(sArr) == SelectionSort(tArr))
+                return true;
+
+            return false;
+        }
+
+        private string InsertionSort(char[] charArr)
+        {
+            for (int i = 1; i < charArr.Length; i++)
+            {
+                for (int j = i; j > 0; j--)
+                    if (charArr[j] < charArr[j - 1])
+                    {
+                        char s = charArr[j];
+                        charArr[j] = charArr[j - 1];
+                        charArr[j - 1] = s;
+                    }
+            }
+
+            return new string(charArr);
+        }
+
+        private string SelectionSort(char[] charArr)
+        {
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                int min = i;
+
+                for (int j = i + 1; j < charArr.Length; j++)
+                {
+                    if (charArr[min] > charArr[j])
+                        min = j;
+                }
+
+                char minChar = charArr[min];
+                charArr[min] = charArr[i];
+                charArr[i] = minChar;
+            }
+
+            return new string(charArr);
+        }
+
+
+        private string EfficientSort(char[] charArr)
+        {
+            Array.Sort(charArr);
+
+            return new string(charArr);
+        }
+    }
+}
---
+++ week02/artem_shlezinger/ValidParentheses.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Stacks
+{
+    class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char ch in s)
+            {
+                char? p = null;
+
+                if (ch == '}' || ch == ')' || ch == ']')
+                {
+                    if (stack.Count > 0)
+                        p = stack.Pop();
+
+                    if(ch == '}' && p != '{' || ch == ')' && p != '(' || ch == ']' && p != '[')
+                        return false;
+                }
+                else
+                    stack.Push(ch);
+
+            }
+
+            if (stack.Count > 0)
+                return false;
+
+            return true;
+        }
+    }
+}
