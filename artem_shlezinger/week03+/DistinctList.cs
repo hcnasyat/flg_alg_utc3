0644aba10d72822fc3a02c3ec9bc9cd116617603
artem2663 <artem2663@gmail.com>
2021-03-12 13:11:46 +0200

    easy tasks

---
+++ artem_shlezinger/week03+/DistinctList.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms
+{
+
+    class DistinctList
+    {
+        internal class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode tempNode = head;
+
+            while (tempNode != null && tempNode.next != null)
+                if (tempNode.val == tempNode.next.val)
+                    tempNode.next = tempNode.next.next;
+                else
+                    tempNode = tempNode.next;
+
+            return head;
+        }
+    }
+}
---
+++ artem_shlezinger/week03+/OutermostParentheses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms.Stacks
+{
+    class OutermostParentheses
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            string res = "";
+
+            foreach (char c in S)
+            {
+                if (c == '(')
+                {
+                    if (stack.Count > 0)
+                        res = String.Concat(res, "(");
+
+                    stack.Push(c);
+                }
+
+                if (c == ')')
+                {
+                    stack.Pop();
+
+                    if (stack.Count > 0)
+                        res = String.Concat(res, ")");
+                }
+            }
+
+            return res;
+        }
+    }
+}
