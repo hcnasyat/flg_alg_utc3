aa8e2cad67334e13fcf88cd019969a1ae4323392
artem2663 <artem2663@gmail.com>
2021-03-24 01:16:49 +0200

    Greater tree

---
+++ artem_shlezinger/week04+/GreaterTree.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.BST;
+
+namespace Algorithms
+{
+    class GreaterTree
+    {
+        int sum = 0;
+
+        public TreeNode BstToGst(TreeNode root)
+        {
+            CalcTree(root);
+
+            return root;
+        }
+
+        public void CalcTree(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            CalcTree(node.right);
+
+            sum += node.val;
+            node.val = sum;
+            //Console.WriteLine(sum);
+
+            CalcTree(node.left);
+        }
+    }
+}
