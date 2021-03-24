eb0cbe6d46fc5e7ebf1a76500cec58169d0c77d3
artem2663 <artem2663@gmail.com>
2021-03-24 11:34:55 +0200

    Kth Smallest Element in a BST

---
+++ artem_shlezinger/week04+/SmallestElement.cs
@@ -0,0 +1,30 @@
+﻿namespace Algorithms.BST
+{
+    class SmallestElement
+    {
+        int i = 0, res, K;
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            K = k;
+            TraversalTree(root);
+            return res;
+        }
+
+        public void TraversalTree(TreeNode node)
+        {
+            if (node == null)
+                return;
+
+            TraversalTree(node.left);
+
+            if (K == i)
+                return;
+
+            res = node.val;
+            i++;
+
+            TraversalTree(node.right);
+        }
+    }
+}
