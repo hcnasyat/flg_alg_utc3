8ef5024336d152a013b2ada7c4260f384774d38b
artem2663 <artem2663@gmail.com>
2021-04-01 22:36:12 +0300

    Balanced Binary Tree

---
+++ artem_shlezinger/week05/BalancedBinaryTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Algorithms.BST
+{
+    class BalancedBinaryTree
+    {
+        bool res = true;
+
+        public bool IsBalanced(TreeNode root)
+        {
+            Traverse(root);
+
+            return res;
+        }
+
+        private int Traverse(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int l = Traverse(root.left);
+
+            int r = Traverse(root.right);
+
+            if (Math.Abs(l - r) > 1)
+                res = false;
+
+            return 1 + Math.Max(l,r);
+        }
+    }
+}
