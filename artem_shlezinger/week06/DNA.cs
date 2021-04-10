3a36d84e6f587de058da197ace36fbec9fdd9f4a
artem2663 <artem2663@gmail.com>
2021-04-10 12:37:55 +0300

    Repeated DNA Sequences

---
+++ artem_shlezinger/week06/DNA.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    class DNA
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            HashSet<string> set = new HashSet<string>();
+            HashSet<string> resSet = new HashSet<string>();
+
+
+            for(int i = 0; i <= s.Length - 10; i++)
+            {
+                string substr = s.Substring(i, 10);
+
+                if (!set.Contains(substr))
+                    set.Add(substr);
+                else if(!resSet.Contains(substr))
+                    resSet.Add(substr);
+            }
+
+            return resSet.ToList(); ;
+        }
+    }
+}
