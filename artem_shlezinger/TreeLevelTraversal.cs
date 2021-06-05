d0085b737e87a265940a0805c043ad1f08a7cd0d
artem2663 <artem2663@gmail.com>
2021-06-05 12:58:10 +0300

    Binary Tree Level Order Traversal

---
+++ artem_shlezinger/TreeLevelTraversal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Algorithms.BST;
+
+namespace Algorithms
+{
+    class TreeLevelTraversal
+    {
+
+        Dictionary<int, IList<int>> dict = new Dictionary<int, IList<int>>();
+        
+		public IList<IList<int>> LevelOrder(TreeNode root)
+		{         
+			Method(root, 0);
+	  
+			var res = dict.OrderBy(k => k.Key).ToDictionary(o => o.Key, o => o.Value).Values.ToList();
+	   
+			return res;
+		}
+
+			
+		void Method(TreeNode node, int level)
+		{
+			if (node == null) 
+				return;
+
+			Method(node.left, level+1);
+
+
+			if (dict.ContainsKey(level))    
+				dict[level].Add(node.val);
+			else     
+			{  
+				var i = new List<int>();  
+				i.Add(node.val);
+					
+				dict.Add(level, i);
+			}
+
+	   
+			Method(node.right, level+1);
+		}
+    }
+}
