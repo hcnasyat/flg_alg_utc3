6071a12f0cc05dd21739d142cd73bc95210e2695
artem2663 <artem2663@gmail.com>
2021-06-12 13:54:01 +0300

    Jump Game

---
+++ artem_shlezinger/JumpGame.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms
+{
+    class JumpGame
+    {
+        public bool CanJump(int[] nums)
+        {
+            int maxJump = nums[0];
+            int maxIdx = 0;
+
+            if (nums[0] == 0 && nums.Length > 1)
+                return false;
+
+            for (int i = 1; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == 0 && i - maxIdx >= maxJump)
+                    return false;
+
+                if (nums[i] + i >= maxJump + maxIdx)
+                {
+                    maxJump = nums[i];
+                    maxIdx = i;
+                }
+            }
+
+            return true;
+        }
+    }
+}
